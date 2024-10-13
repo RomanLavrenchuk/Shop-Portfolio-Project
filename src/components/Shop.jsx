6d082c15ae9c7ee9e64ec8e8bd81047462a1a02d
Roman <polskaago@gmail.com>
2024-10-13 11:27:36 -0400

    Basket product adding and Price calculating

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,12 +3,13 @@ import { API_KEY, API_URL } from '../config';
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
 import { Cart } from './Cart';
+import { BasketList } from './BasketList';
 
 function Shop() {
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
     const [order, setOrder] = useState([]);
-    console.log(order);
+    const [isBasketShow, setBasketShow] = useState(false);
 
     const addToBasket = (item) => {
         const itemIndex = order.findIndex(
@@ -29,11 +30,15 @@ function Shop() {
                         quantity: orderItem.quantity + 1,
                     };
                 } else {
-                    return item;
+                    return orderItem;
                 }
             });
             setOrder(newOrder);
         }
+    };
+
+    const handleBasketShow = () => {
+        setBasketShow(!isBasketShow);
     };
 
     useEffect(function getGoods() {
@@ -49,11 +54,14 @@ function Shop() {
 
     return (
         <main className='container content'>
-            <Cart quantity={order.length} />
+            <Cart quantity={order.length} handleBasketShow={handleBasketShow} />
             {loading ? (
                 <Preloader />
             ) : (
                 <GoodsList goods={goods} addToBasket={addToBasket} />
+            )}
+            {isBasketShow && (
+                <BasketList order={order} handleBasketShow={handleBasketShow} />
             )}
         </main>
     );
